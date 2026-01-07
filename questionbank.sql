5d96edc0382a99b13420a949029e526ed2094f7a
rutujamokale <rutujamokale2003@gmail.com>
2026-01-07 12:44:20 +0530

    questionbank query

--- questionbank.sql
+++ questionbank.sql
@@ -1,23 +1,28 @@
+-- list all questions for a subject and concept --
 select questionbank.title 
 from questionbank
 inner join  subject_concepts as sc on sc.subject_concept_id=questionbank.subject_concept_id
 inner join subjects as s on  s.id= sc.subject_id where s.id=1; 
 
+
+-- list all questions for  a subject with level --
 select questionbank.title 
 from questionbank
 inner join  subject_concepts as sc on sc.subject_concept_id=questionbank.subject_concept_id
 inner join subjects as s on  s.id= sc.subject_id where s.id=1 and questionbank.difficulty_level="Beginner";
 
-
+-- list all questions for  a subject,concept  with level
 select  qb.id,qb.title,qb.a,qb.b ,qb.c,qb.d ,qb.answerkey,qb.difficulty_level
 from questionbank qb
 join subject_concepts sc on sc.subject_concept_id=qb.subject_concept_id
 join subjects s on s.id= sc.subject_id  and difficulty_level="beginner" and
 sc.concept_id=1;
 
+-- list all questions created by--
 select title,a,b,c,d ,answerkey,created_by from questionbank
 where created_by="Ravi Tambade";
 
+-- list all questions for created by subject
 select qb.title,qb.a,qb.b,qb.c,qb.d,qb.answerkey
 from questionbank qb
 join subject_concepts sc on sc.subject_concept_id=qb.subject_concept_id
@@ -26,7 +31,7 @@ where s.title="Dotnet" and
 created_by="Sanika Bhor";
 
 
-
+-- list all questions subject,concept ,created by --
 select qb.title,qb.a,qb.b,qb.c,qb.d,qb.answerkey
 from questionbank qb
 join subject_concepts sc on sc.subjectc_concept_id=qb.subject_concept_id
@@ -34,7 +39,18 @@ join subjects s on  sc.subject_id=s.id
 join concepts c on sc.subject_concept_id=c.id
 where s.title="Dotnet" and
 created_by="Sanika Bhor"
+and c.id=1;
+
+-- list all questions subject.concept,create by,level --
+select qb.title,qb.a,qb.b,qb.c,qb.d,qb.answerkey
+from questionbank qb
+join subject_concepts sc on sc.subject_concept_id=qb.subject_concept_id
+join subjects s on  sc.subject_id=s.id
+join concepts c on sc.subject_concept_id=c.id
+where s.title="Dotnet" and
+qb.created_by="Sanika Bhor"
 and c.id=1
+and qb.difficulty_level="beginner";
 
 
 
