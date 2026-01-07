74d2ba7280c2c7489eb54407540642014f95ee97
rutujamokale <rutujamokale2003@gmail.com>
2026-01-07 12:32:47 +0530

    questionbank query

--- questionbank.sql
+++ questionbank.sql
@@ -15,3 +15,28 @@ join subject_concepts sc on sc.subject_concept_id=qb.subject_concept_id
 join subjects s on s.id= sc.subject_id  and difficulty_level="beginner" and
 sc.concept_id=1;
 
+select title,a,b,c,d ,answerkey,created_by from questionbank
+where created_by="Ravi Tambade";
+
+select qb.title,qb.a,qb.b,qb.c,qb.d,qb.answerkey
+from questionbank qb
+join subject_concepts sc on sc.subject_concept_id=qb.subject_concept_id
+join subjects s on  sc.subject_id=s.id
+where s.title="Dotnet" and
+created_by="Sanika Bhor";
+
+
+
+select qb.title,qb.a,qb.b,qb.c,qb.d,qb.answerkey
+from questionbank qb
+join subject_concepts sc on sc.subjectc_concept_id=qb.subject_concept_id
+join subjects s on  sc.subject_id=s.id
+join concepts c on sc.subject_concept_id=c.id
+where s.title="Dotnet" and
+created_by="Sanika Bhor"
+and c.id=1
+
+
+
+
+
